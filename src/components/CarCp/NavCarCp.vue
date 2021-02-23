54f8154f2e4eeb1822a12166c43108785b69cbbc
rahim einollahi <r.einollahi1@gmal.com>
2021-02-24 00:16:33 +0330

    add nav recovery completed

---
+++ src/components/CarCp/NavCarCp.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="continer">
+        <div class="d-flex justify-content-center mt-3 ">
+            <button class="btn btn-warning btn-lg rounded  text-white border">DONATE</button>
+
+            <router-link class="icon-margin" to="/carhome"><i class="fa fa-instagram" style="font-size:36px; color:red"></i></router-link>
+            <router-link class="icon-margin" to="/carhome"><i class="fa fa-twitter" aria-hidden="true" style="font-size:36px;"></i></router-link>
+            <router-link class="icon-margin" to="/carhome"><i class="fa fa-facebook" style="font-size:36px; color:red"></i></router-link>
+
+            <input type="text" placeholder="Email Address" class="bg-dark mr-3 ml-3 rounded text-white nav-link mt-2 form-control">
+            <button class="bg-info nav-link text-white font-weight-bold rounded mr-3 ml-3">GET UPDATED</button>
+        </div>
+
+    </div>  
+</template>
+
+<script>
+    export default {
+        name:'NavCarCp'
+    }
+</script>
+
+<style lang="scss" scoped>
+.btn-bg-color{
+    background-color: rgb(236, 10, 217);
+    font-size: 2rem;
+    font-weight: bold;
+}
+.icon-margin{
+    width: 40px;
+    height: 40px;
+    margin-right: 10px;
+    margin-left: 10px;
+}
+.inputemail:focus{
+    text-shadow: 5px 8px 12px red;
+    color: blue;
+}
+</style>
--- src/views/CarView/CarHome.vue
+++ src/views/CarView/CarHome.vue
@@ -1,12 +1,16 @@
 <template>
-    <div>
-        <p>car home pge..</p>
+    <div class="continer">
+        <nav-car-cp />
     </div>
 </template>
 
 <script>
+import NavCarCp from '../../components/CarCp/NavCarCp.vue'
     export default {
-        name:'CarHome'
+        name:'CarHome',
+        components:{
+            NavCarCp
+        }
     }
 </script>
 
