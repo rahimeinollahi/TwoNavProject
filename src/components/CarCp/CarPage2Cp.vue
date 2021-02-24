74b364665bb6f7cfafa4fee99b6df96ded2e7cd7
rahim einollahi <r.einollahi1@gmal.com>
2021-02-24 22:52:55 +0330

    add carhome page2 completed

--- src/components/CarCp/CarPage2Cp.vue
+++ src/components/CarCp/CarPage2Cp.vue
@@ -1,15 +1,38 @@
 <template>
-    <div>
-        <p>test car pagew two hani ba</p>
+    <div class="continer">
+        <div  class="row">
+            <div  class="col-md-4 mt-1" v-for="item in getDataPage2" :key="item.id">
+                <div id="image-gallery-page2" class="thumbnail" >
+                            <div clcard p-1>
+                            <img class="img-fluid " :src="item.url" alt="item.alt" style="width:100%">
+                                <div id="image-btn" class="caption list-group-item list-group-item-action ">
+                                    <p class="text-center ">{{item.text}}}</p>
+                                </div>
+                        </div>
+                </div>
+            </div>
+         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name:'CarPage2Cp'
+        name:'CarPage2Cp',
+        computed:{
+            getDataPage2()
+            {
+                return this.$store.state.carIndex.carPage2;
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-
+#image-gallery-page2:hover{
+box-shadow: 2px 2px 8px blue;
+}
+img{
+height: 180px;
+object-fit: fill;
+}
 </style>
--- src/store/Modules/CarIndex.js
+++ src/store/Modules/CarIndex.js
@@ -6,6 +6,17 @@ const state={
         {id:4,headerName:"Certificates",text:"When a server receives an HTTPS Web page request, it invokes one of two security software systems, Secure Sockets Layer or the newer Transport Layer Security. SSL and TLS reside on your PC and the server; it is these programs that perform the actual encryption and decryption of information. For example, during an online banking session, you fill out a Web form to see your current balance. The browser passes the page to TLS, which encrypts the data and sends it to the bank's server through the Internet. The bank's computer receives the data and uses TLS to decrypt it, after which the server processes the request."},
         {id:5,headerName:" For testing 1",text:"Communications over the Internet rely on protocols that format your data and dictate how it's used. For example, programs use the File Transfer Protocol to upload files to a Web server and perform maintenance tasks such as renaming files and creating directories. Browser programs such as Firefox and Internet Explorer use the Hyper Text Transfer Protocol, or HTTP, to load most Web pages. HTTP, however, is not secure; a motivated hacker can monitor your data traffic and see which Web pages you visit. To solve this problem, network engineers developed the HTTPS protocol, which moves data using secure methods"},
         {id:6,headerName:" For testing 2",text:"Communications over the Internet rely on protocols that format your data and dictate how it's used. For example, programs use the File Transfer Protocol to upload files to a Web server and perform maintenance tasks such as renaming files and creating directories. Browser programs such as Firefox and Internet Explorer use the Hyper Text Transfer Protocol, or HTTP, to load most Web pages. HTTP, however, is not secure; a motivated hacker can monitor your data traffic and see which Web pages you visit. To solve this problem, network engineers developed the HTTPS protocol, which moves data using secure methods"},
+    ],
+    carPage2:[
+        {id:1,url:'/../../imagebook/1.jpg',text:'book 1'},
+        {id:2,url:'/../../imagebook/2.jpg',text:'book 2'},
+        {id:3,url:'/../../imagebook/3.jpg',text:'book 3'},
+        {id:4,url:'/../../imagebook/4.jpg',text:'book 4'},
+        {id:5,url:'/../../imagebook/5.jpg',text:'book 5'},
+        {id:6,url:'/../../imagebook/6.jpg',text:'book 6'},
+        {id:7,url:'/../../imagebook/7.jpg',text:'book 7'},
+        {id:8,url:'/../../imagebook/8.jpg',text:'book 8'},
+        {id:9,url:'/../../imagebook/2.jpg',text:'book 9'},
     ]
 };
 
